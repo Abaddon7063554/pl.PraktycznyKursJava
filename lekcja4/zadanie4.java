1852f25c29fb48601538ddc22fdd17cff1ca453e
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-07-23 18:40:08 +0200

    Create zadanie4.java

---
+++ lekcja4/zadanie4.java
@@ -0,0 +1,9 @@
+package lekcja4;
+public class zadanie4 {
+    public static void main(String[] args) {
+        short pierwsza = 32134 ;
+        short druga = 3214;
+        System.out.println(pierwsza+druga);
+        System.out.println("pierwsza = "+pierwsza + " druga = "+druga);
+    }
+}
