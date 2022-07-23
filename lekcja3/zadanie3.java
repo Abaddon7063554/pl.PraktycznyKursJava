ad940dd6d8974a0613239262275021ae13d8ac02
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-07-23 18:26:17 +0200

    Create zadanie3.java

---
+++ lekcja3/zadanie3.java
@@ -0,0 +1,10 @@
+package lekcja3;
+public class zadanie3{
+    public static void main(String[] args) {
+        System.out.println("komentarze ");
+        // pojedyncza linia
+        /*
+         * Blokowy komentarz
+         */
+    }
+}
