4c4fca0a8f98ecb914f26770017bd052b3bbaae7
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-07-23 18:09:02 +0200

    Update zadanie1.java

--- lekcja1/zadanie1.java
+++ lekcja1/zadanie1.java
@@ -1,8 +1,7 @@
 package lekcja1;
-
 public class zadanie1{
     public static void main(String[] args) {
-        System.out.print("To mój drugi program12");
+        System.out.print("To mój drugi program");
     }
     
 }
