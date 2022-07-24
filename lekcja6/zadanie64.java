5fa5ce7105ace4b69eabea659bac0b525a578042
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-07-24 11:16:59 +0200

    Update zadanie64.java

--- lekcja6/zadanie64.java
+++ lekcja6/zadanie64.java
@@ -1,12 +1,9 @@
 package lekcja6;
-
 public class zadanie64 {
     public static void main(String [] args){
         int i=55;
         int j=12;
         int wynik =i^j;
-        
-        System.out.println("przypisanie do zmiennej = "+wynik
-        +" \n zastosowanie ^= " +(i^=j));
+        System.out.println("przypisanie do zmiennej = "+wynik+" \n zastosowanie ^= " +(i^=j));
     }
 }
