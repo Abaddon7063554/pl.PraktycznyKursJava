e50afd7b604002c5372667ab3b2c7359d65c19c9
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-07-26 22:45:16 +0200

    Abaddon24

--- lekcja3/zadanie3.java
+++ lekcja3/zadanie3.java
@@ -1,9 +1,9 @@
 package lekcja3;
 public class zadanie3{
     public static void main(String[] args) {
-        System.out.println("komentarze ");
+        System.out.println("komentarze");
         // pojedyncza linia
-        /*
+        /*  
          * Blokowy komentarz
          */
     }
