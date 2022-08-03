53a4ad41a36280c076e2d8efd7c1f128b75fa08f
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-08-03 18:58:22 +0200

    Create zadanie14_1.java

---
+++ lekcja14/zadanie14_1.java
@@ -0,0 +1,10 @@
+package lekcja14;
+
+public class zadanie14_1 {
+    public static void main(String arg[]){
+        for(int i =0 ; i<arg.length;i++){
+            System.out.println(arg[i]);
+            System.out.println("1");
+        }
+    }
+}
