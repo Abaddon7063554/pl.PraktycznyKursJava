9a77df4c4b9cbf109fd63ba29fe2899e3fa352af
Abaddon7063554 <lukasz.kurek@o2.pl>
2022-10-15 15:27:10 +0200

    Abaddon24

--- ftp/polaczenie.java
+++ ftp/polaczenie.java
@@ -1,30 +1,22 @@
 package ftp;
 import java.io.*;
-
 import org.apache.commons.net.ftp.FTPClient;
-
 public class polaczenie{
 public static void main(String [] args)
     {
+        System.out.println("mam cie w dupiue");
         String server = "ftp://192.168.210.29";
         int port = 21;
         String user = "login";
         String pass = "haslo";
         FTPClient ftpClient = new FTPClient();
         try {
- 
             ftpClient.connect(server, port);
             ftpClient.login(user, pass);
             ftpClient.enterLocalPassiveMode();
- 
             //ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
- 
-            // APPROACH #1: uploads first file using an InputStream
-            
- 
+            // APPROACH #1: uploads first file using an InputStream         
             // APPROACH #2: uploads second file using an OutputStream
-           
-           
         } catch (IOException ex) {
             System.out.println("Error: nie polaczono" + ex.getMessage());
             ex.printStackTrace();
@@ -34,7 +26,8 @@ public static void main(String [] args)
                     ftpClient.logout();
                     ftpClient.disconnect();
                 }
-            } catch (IOException ex) {
+            } 
+            catch (IOException ex) {
                 ex.printStackTrace();
             }
         }
